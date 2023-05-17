c75ef5f4f96658a08b22818af7e886d9f65c2019
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-05-17 19:34:06 +0300

    Servis.api-app_update

---
+++ src/servise/api.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+const apiKey = 'a7b06e7a05ba5e2eb883b74f2a4c575a';
+
+//trending/get-trending список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+
+export async function getTrendingMovies() {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&include_adult=false`
+    );
+    return data;
+  } catch (error) {
+    throw new Error('');
+  }
+}
+//search/search-movies пошук фільму за ключовим словом на сторінці фільмів.
+
+export async function getMovieByQuery(query) {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key${apiKey}&query=${query}&page=1&include_adult=false`
+    );
+    return data;
+  } catch (error) {
+    throw new Error('');
+  }
+}
+
+//movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
+
+export async function getMovieDetails(movieId) {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`
+    );
+
+    return data;
+  } catch (error) {
+    throw new Error('');
+  }
+}
+
+//movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
+
+export const getMoviesCredits = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
+    );
+    // console.log(data.credits);
+    return data.credits;
+  } catch (error) {
+    throw new Error('');
+  }
+};
+
+//movies/get-movie-reviews запит оглядів для сторінки кінофільму.
+
+export const getMoviesReviews = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${apiKey}`
+    );
+    return data.results;
+  } catch (error) {
+    throw new Error('');
+  }
+};
